a38f76def6ecdb1b3a9364e5a71c241b0539cfeb
Yuzuru-E <yzryzr88@gmail.com>
2020-09-12 13:21:33 +0900

    一覧表示ページ更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :items
+  resources :items do
+    collection do
+      get 'item_list'
+      get 'brand_index'
+    end
+  end
   resources :favorites, only: :index
   resources :reviews, only: :index
 end
