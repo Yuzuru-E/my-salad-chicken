2d713522ed42e8810ff7821e2330a1488db626f4
Yuzuru-E <yzryzr88@gmail.com>
2020-09-13 10:09:06 +0900

    ユーザー登録機能作成初期ファイル

--- db/migrate/20200912072234_devise_create_users.rb
+++ db/migrate/20200912072234_devise_create_users.rb
@@ -7,8 +7,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name, null: false
-      t.references :gender, null: false
-      t.references :age, null: false
+      t.string :nickname, null: false
+      t.references :gender
+      t.references :age
       t.text :avaterImg
       t.text :introduction
       ## Recoverable
