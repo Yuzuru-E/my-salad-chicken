29cdf4d57fde3727961f379ca70c0e9417215676
Yuzuru-E <yzryzr88@gmail.com>
2020-09-23 22:25:47 +0900

    Update gender.rb

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,9 +1,10 @@
 class Gender < ActiveHash::Base
+  include ActiveHash::Associations
+  has_many :users
+  
   self.data = [
     {id: 1, gender: '男性'}, {id: 2, gender: '女性'},
     {id: 3, gender: 'その他'}, {id: 4, gender: '回答しない'}
   ]
-  include ActiveHash::Associations
-  has_many :users
   
 end
