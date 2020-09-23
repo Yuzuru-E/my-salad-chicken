274800c0c5e9958ba49140d7c2ce4841b88aa22a
Yuzuru-E <yzryzr88@gmail.com>
2020-09-23 22:25:28 +0900

    Update age.rb

--- app/models/age.rb
+++ app/models/age.rb
@@ -1,10 +1,11 @@
 class Age < ActiveHash::Base
+  include ActiveHash::Associations
+  has_many :users
+
   self.data = [
     {id: 1, age: '10代'}, {id: 2, age: '20代'}, {id: 3, age: '30代'},
     {id: 4, age: '40代'}, {id: 5, age: '50代'}, {id: 6, age: '60代'},
     {id: 7, age: '70代以上'}, 
   ]
-  include ActiveHash::Associations
-  has_many :users
   
 end
