28ab32246039d4384d163f1d09d352fac27463a9
Yuzuru-E <yzryzr88@gmail.com>
2020-10-03 23:55:34 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,17 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :check_guest, only: %i[update destroy]
+  before_action :check_guest, only: :destroy
+
+  
+
+  
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+  
+  def after_update_path_for(resource)
+    user_path(@user.id)
+  end
+
 end
