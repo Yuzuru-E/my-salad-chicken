7f6cd7d45d9ff67ffb1be176bdd4c35d7bb0a620
Yuzuru-E <yzryzr88@gmail.com>
2020-10-03 23:58:59 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
+  before_action
 
   def new
   end
 
-  def edit
+  def show
+    @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(current_user.id), notice: 'ユーザー情報が更新されました。'
+    
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:avaterImg, :name, :nickname, :age_id, :gender_id)
+  end
+
 end
