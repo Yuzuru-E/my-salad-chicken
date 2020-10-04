12d18f554b1e7142376601c84fe2f2e6c0db9458
Yuzuru-E <yzryzr88@gmail.com>
2020-10-04 23:45:19 +0900

    avaterImg投稿可能に修正

--- app/uploaders/avaterImg_uploader.rb
+++ app/uploaders/avaterImg_uploader.rb
@@ -1,4 +1,4 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class AvaterImgUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
