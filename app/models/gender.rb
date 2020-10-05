725cbc0d17324c4c5ff81c35296a6299dd650a8c
Yuzuru-E <yzryzr88@gmail.com>
2020-10-05 22:52:47 +0900

    Update gender.rb

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -4,7 +4,7 @@ class Gender < ActiveHash::Base
   
   self.data = [
     {id: 0, gender: '--'}, {id: 1, gender: '女性'},
-    {id: 2, gender: '男性'}, {id: 3, gender: 'その他　'}
+    {id: 2, gender: '男性'}, {id: 3, gender: 'その他'}
   ]
   
 end
