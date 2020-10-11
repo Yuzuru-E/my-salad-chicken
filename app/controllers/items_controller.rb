b8446e0ac9d2d90596c3c744c025364e8e8dd922
Yuzuru-E <yzryzr88@gmail.com>
2020-10-11 17:46:23 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,12 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @user = User.find(current_user.id)
     @item = Item.find(1)
+    @review = Review.find(1)
+  end
+
+  def new
   end
 
 end
