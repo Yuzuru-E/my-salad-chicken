6beffdd54534bc9c0a12cb95b3ce26e50b3c099f
Yuzuru-E <yzryzr88@gmail.com>
2020-10-13 23:57:15 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,12 +4,20 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    review = Review.create(review_params)
+    redirect_to "/items/#{review.item.id}"
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:reviewTitle, :evaluation, :comment, :commentImg).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
