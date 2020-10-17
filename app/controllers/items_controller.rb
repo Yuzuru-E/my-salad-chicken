4769d399998a583ba4400deddd9bd3a8e99eb895
Yuzuru-E <yzryzr88@gmail.com>
2020-10-17 23:23:17 +0900

    口コミ投稿機能追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,30 @@
 class ItemsController < ApplicationController
-
+  before_action :set_item, only: [:edit, :show]
   def index
+    @item = Item.find(1)
   end
 
   def show
     @user = User.find(current_user.id)
-    @item = Item.find(1)
-    @review = Review.find(1)
+    @review = Review.new
+    @reviews = @item.reviews.includes(:user)
   end
 
   def new
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:reviewTitle, :evaluation, :comment, :commentImg).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
