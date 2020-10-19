74dc0f0a525847bab5ac8bd9a1f5a17a82be9794
Yuzuru-E <yzryzr88@gmail.com>
2020-10-19 17:16:34 +0900

    makerカラム修正

--- db/migrate/20200905060044_create_items.rb
+++ db/migrate/20200905060044_create_items.rb
@@ -2,9 +2,9 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :name, null: false
-      t.string :maker, null: false
+      t.string :maker_id, null: false, foreign_key: true
       t.text :maker_url, null: false
-      t.integer :price, null: false  
+      t.integer :price, null: false
       t.timestamps
     end
   end
