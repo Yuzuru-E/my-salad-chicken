066729a7d1b4d402727c2bc0d114edcd45319c40
Yuzuru-E <yzryzr88@gmail.com>
2020-10-19 17:50:57 +0900

    キー名修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -28,7 +28,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:reviewTitle, :evaluation, :comment, :commentImg).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:review).permit(:title, :evaluation, :comment, :commentImg).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_review
