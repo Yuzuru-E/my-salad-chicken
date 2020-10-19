7cf42cd5974c94fe7ddc2bcc07d542e7662bc747
Yuzuru-E <yzryzr88@gmail.com>
2020-10-19 15:04:29 +0900

    口コミ変更及び削除機能追記

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-
+  before_action :set_review, only: [:edit, :update, :destroy]
   def index
   end
 
@@ -8,16 +8,31 @@ class ReviewsController < ApplicationController
     redirect_to "/items/#{review.item.id}"
   end
 
+  def show
+  end
+
   def edit
+    @item = Item.find(params[:item_id])
   end
 
   def destroy
+    @review.destroy
+    redirect_to "/items/#{@review.item_id}"
+  end
+
+  def update
+    @review.update(review_params)
+    redirect_to "/items/#{@review.item_id}"
   end
 
   private
 
   def review_params
     params.require(:review).permit(:reviewTitle, :evaluation, :comment, :commentImg).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
   end
 
 end
