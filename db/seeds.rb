93802d0c23331e6832de0897b086b6f76aa1b79a
Yuzuru-E <yzryzr88@gmail.com>
2020-10-20 22:18:18 +0900

    管理者権限ユーザーのパスワードの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ User.create!(
   name:  "管理者",
   nickname: "管理者",
   email: "admin@mysaladchicken.jp",
-  password:  "adminuser",
-  password_confirmation: "adminuser",
+  password:  "admin00user",
+  password_confirmation: "admin00user",
   age_id: 0,
   gender_id: 0,
   admin: true)
