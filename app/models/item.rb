23f19154fb3930ef52ccfa256fd99954632ee426
Yuzuru-E <yzryzr88@gmail.com>
2020-10-21 13:31:42 +0900

    データベース修正の為

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :maker
   has_many :reviews
   has_many :users, through: :reviews
+
+  mount_uploader :image_url, ItemImageUploader
 end
--- app/models/item_Image.rb
+++
@@ -1,6 +0,0 @@
-class ItemImage < ApplicationRecord
-  belongs_to :item
-  validates :url, presence: true
-
-  mount_uploader :url, ItemImageUploader
-end
